4baeeb5022c4d12d078add0e15c617b188b20b15
Stephane <stephane.achari@u-psud.fr>
2018-12-10 22:44:44 +0100

    searche autocomplete

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SearchBar.css';
 import { withRouter } from 'react-router-dom';
-import { IoIosSearch } from 'react-icons/io';
-import { API_URL, API_KEY, MOVIE_POPULAR } from '../DataConfig';
+import { IoIosSearch, IoIosCloudyNight } from 'react-icons/io';
+import { API_URL, API_KEY } from '../DataConfig';
 
 class SearchBar extends React.Component {
   state = {
     searchField: '',
-    srchInput: '',
+    srchInput: [],
   };
 
   handleEntree = event => {
@@ -16,31 +16,37 @@ class SearchBar extends React.Component {
         this.props.history.push(`/film/research/${this.state.searchField}`);
         this.setState({ searchField: '' });
       }
-      // } else if (this.state.searchField.length >= 2) {
-      //   this.getMoviePopular();
-      // }
     }
   };
 
-  // getMoviePopular = () => {
-  //   let inputRequest = `${API_URL}${MOVIE_POPULAR}${API_KEY}&page=${this.state
-  //     .actualPage + 1}`;
-
-  //   fetch(inputRequest)
-  //     .then(result => result.json())
-  //     .then(result => {
-  //       console.log(result); //test data,
-  //       this.setState(prevState => ({
-  //         //on ajoute les nouveaux films aux films qu'on a déjà
-  //         actualPage: result.page,
-  //         totalPageRequest: result.total_pages,
-  //         fetching: false,
-  //       }));
-  //     });
-  // };
-
   handleInputChange = event => {
     this.setState({ searchField: event.target.value });
+    if (event.target.value.length >= 1) {
+      this.getMoviePopular();
+    }
+  };
+
+  getMoviePopular = () => {
+    let inputRequest = `${API_URL}search/movie${API_KEY}&query=${
+      this.state.searchField
+    }&page=1&include_adult=false`;
+
+    fetch(inputRequest)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result);
+        this.putInFormInfo(result.results);
+      });
+  };
+
+  putInFormInfo = list => {
+    // Récupère la liste des titre
+    this.setState({
+      srchInput: list.map(elt => {
+        return elt.title;
+      }),
+    });
+    console.log(this.state.srchInput);
   };
 
   // handleClick = event => {
@@ -48,17 +54,31 @@ class SearchBar extends React.Component {
   // };
 
   render() {
+    const { searchField, srchInput } = this.state;
+
     return (
       <React.Fragment>
         <input
           className="searchBar"
           type="text"
           placeholder="Search.."
-          value={this.state.searchField}
+          value={searchField}
           onChange={this.handleInputChange}
           onKeyPress={this.handleEntree}
-          list={this.state.srchInput}
+          list="srchInputList"
+          name="srchInputList"
         />
+
+        {srchInput.length > 0 ? (
+          <datalist id="srchInputList">
+            {srchInput.map(item => {
+              return <option value={item} />;
+            })}
+          </datalist>
+        ) : (
+          ''
+        )}
+
         <button
           id="btnReSearch"
           className="btnSearch"
