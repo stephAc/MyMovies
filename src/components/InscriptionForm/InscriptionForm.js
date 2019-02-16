f1b480b7949acf2d0f13661df3d178194fac8624
Stephane <stephane.achari@u-psud.fr>
2019-02-16 16:59:30 +0100

    Regex Inscription

--- src/components/InscriptionForm/InscriptionForm.js
+++ src/components/InscriptionForm/InscriptionForm.js
@@ -9,28 +9,65 @@ export default class InscriptionForm extends Component {
     email: '',
     pwd: '',
     confirmedPwd: '',
+    patterns: {
+      name: /^.{4,12}$/,
+      email: /.+@.+.(com|fr|co.uk)$/,
+      pwd: /^((?=.*\d)(?=.*[A-Z])(?=.*[a-z]).{8,16})$/,
+      confirmedPwd: /^((?=.*\d)(?=.*[A-Z])(?=.*[a-z]).{8,16})$/,
+    },
   };
 
   handleInput = event => {
     const name = event.target.name;
+    const value = event.target.value;
+
+    this.validate(name, this.state.patterns[name], value);
+
     this.setState({
-      [name]: event.target.value,
+      [name]: value,
     });
+  };
+
+  validate = (name, regex, value) => {
+    if (regex.test(value)) {
+      document.getElementsByName(name)[0].className = 'validInput';
+    } else {
+      document.getElementsByName(name)[0].className = 'invalidInput';
+    }
   };
 
   checkForm = event => {
     event.preventDefault();
-    console.log(this.state.pwd + ' ' + this.state.confirmedPwd);
-    if (this.state.pwd === this.state.confirmedPwd) {
-      this.handleSubmit(event);
+    let conform = true;
+    let inputs = document
+      .getElementById('formInscription')
+      .getElementsByTagName('input');
+
+    console.log(inputs.length);
+    for (let i = 0; i < inputs.length; i++) {
+      console.log(inputs[i].name);
+      if (conform) {
+        conform = inputs[i].className === 'validInput' ? true : false;
+      }
+    }
+
+    console.log(conform);
+    if (conform) {
+      if (this.state.pwd === this.state.confirmedPwd) {
+        this.handleSubmit(event);
+      } else {
+        document.getElementById('inputPwdInscription').className =
+          'invalidInput';
+        document.getElementById('inputConfirmedPwdInscription').className =
+          'invalidInput';
+        alert('Mot de passe non confirmé');
+      }
     } else {
-      console.log('incorrect');
+      alert('Champ(s) mal saisis');
     }
   };
 
   handleSubmit = event => {
-    console.log('handle');
-
     const FORM_DATA = new FormData(event.target);
     let jsonObject = {};
     for (const [key, value] of FORM_DATA.entries()) {
@@ -56,44 +93,72 @@ export default class InscriptionForm extends Component {
       <Fragment>
         <BtnSlideBar />
         <Log />
-        <form className="displayFormInscripton" onSubmit={this.checkForm}>
+        <form
+          id="formInscription"
+          className="displayFormInscripton"
+          onSubmit={this.checkForm}
+        >
           <h2 className="titleInscription">Inscription</h2>
-          <input
-            className="inputStyleInscription"
-            type="text"
-            name="name"
-            placeholder="Nom"
-            value={this.state.name}
-            required
-            onChange={this.handleInput}
-          />
-          <input
-            className="inputStyleInscription"
-            type="email"
-            name="email"
-            value={this.state.email}
-            placeholder="E-mail"
-            required
-            onChange={this.handleInput}
-          />
-          <input
-            className="inputStyleInscription"
-            type="password"
-            name="pwd"
-            value={this.state.pwd}
-            placeholder="Password"
-            required
-            onChange={this.handleInput}
-          />
-          <input
-            className="inputStyleInscription"
-            type="password"
-            name="confirmedPwd"
-            value={this.state.confirmedPwd}
-            placeholder="Confirme password"
-            required
-            onChange={this.handleInput}
-          />
+          <div>
+            <input
+              className="inputStyleInscription"
+              type="text"
+              name="name"
+              placeholder="Nom"
+              value={this.state.name}
+              required
+              onChange={this.handleInput}
+            />
+            <span className="obligatoire">*</span>
+            <span className="spanInfo">4 - 12 caractères</span>
+          </div>
+          <div>
+            <input
+              className="inputStyleInscription"
+              type="email"
+              name="email"
+              value={this.state.email}
+              placeholder="E-mail"
+              required
+              onChange={this.handleInput}
+            />
+            <span className="obligatoire">*</span>
+            <span className="spanInfo">
+              De la forme ....@... com | fr | co.uk
+            </span>
+          </div>
+          <div>
+            <input
+              id="inputPwdInscription"
+              className="inputStyleInscription"
+              type="password"
+              name="pwd"
+              value={this.state.pwd}
+              placeholder="Password"
+              required
+              onChange={this.handleInput}
+            />
+            <span className="obligatoire">*</span>
+            <span className="spanInfo">
+              1 Maj, 1 chiffre, 8 - 20 caractères
+            </span>
+          </div>
+          <div>
+            <input
+              id="inputConfirmedPwdInscription"
+              className="inputStyleInscription"
+              type="password"
+              name="confirmedPwd"
+              value={this.state.confirmedPwd}
+              placeholder="Confirme password"
+              required
+              onChange={this.handleInput}
+            />
+            <span className="obligatoire">*</span>
+            <span className="spanInfo">
+              1 Maj, 1min, 1 chiffre, 8 - 20 caractères
+            </span>
+          </div>
           <button className="btnSub">S'inscrire</button>
         </form>
       </Fragment>
