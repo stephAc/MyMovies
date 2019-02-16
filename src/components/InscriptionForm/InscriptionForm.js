b9bd3afca47f5e4fe99410aa733fc85f958025d2
Stephane <stephane.achari@u-psud.fr>
2019-02-16 18:33:54 +0100

    inscription auto connexion

--- src/components/InscriptionForm/InscriptionForm.js
+++ src/components/InscriptionForm/InscriptionForm.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import BtnSlideBar from '../BtnSlideBar/BtnSlideBar';
 import Log from '../Login/Log';
 import './InscriptionForm.css';
+import MyContext from '../../contexts/context';
 
 export default class InscriptionForm extends Component {
   state = {
@@ -36,131 +37,84 @@ export default class InscriptionForm extends Component {
     }
   };
 
-  checkForm = event => {
-    event.preventDefault();
-    let conform = true;
-    let inputs = document
-      .getElementById('formInscription')
-      .getElementsByTagName('input');
-
-    console.log(inputs.length);
-    for (let i = 0; i < inputs.length; i++) {
-      console.log(inputs[i].name);
-      if (conform) {
-        conform = inputs[i].className === 'validInput' ? true : false;
-      }
-    }
-
-    console.log(conform);
-    if (conform) {
-      if (this.state.pwd === this.state.confirmedPwd) {
-        this.handleSubmit(event);
-      } else {
-        document.getElementById('inputPwdInscription').className =
-          'invalidInput';
-        document.getElementById('inputConfirmedPwdInscription').className =
-          'invalidInput';
-        alert('Mot de passe non confirmé');
-      }
-    } else {
-      alert('Champ(s) mal saisis');
-    }
-  };
-
-  handleSubmit = event => {
-    const FORM_DATA = new FormData(event.target);
-    let jsonObject = {};
-    for (const [key, value] of FORM_DATA.entries()) {
-      jsonObject[key] = value;
-    }
-
-    fetch('http://localhost:4000/mymovies/inscription', {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(jsonObject),
-    })
-      .then(() => {
-        window.location.replace('http://localhost:3000/');
-      })
-      .catch(err => console.log('fetch error ' + err.message));
-  };
-
   render() {
     return (
       <Fragment>
         <BtnSlideBar />
         <Log />
-        <form
-          id="formInscription"
-          className="displayFormInscripton"
-          onSubmit={this.checkForm}
-        >
-          <h2 className="titleInscription">Inscription</h2>
-          <div>
-            <input
-              className="inputStyleInscription"
-              type="text"
-              name="name"
-              placeholder="Nom"
-              value={this.state.name}
-              required
-              onChange={this.handleInput}
-            />
-            <span className="obligatoire">*</span>
-            <span className="spanInfo">4 - 12 caractères</span>
-          </div>
-          <div>
-            <input
-              className="inputStyleInscription"
-              type="email"
-              name="email"
-              value={this.state.email}
-              placeholder="E-mail"
-              required
-              onChange={this.handleInput}
-            />
-            <span className="obligatoire">*</span>
-            <span className="spanInfo">
-              De la forme ....@... com | fr | co.uk
-            </span>
-          </div>
-          <div>
-            <input
-              id="inputPwdInscription"
-              className="inputStyleInscription"
-              type="password"
-              name="pwd"
-              value={this.state.pwd}
-              placeholder="Password"
-              required
-              onChange={this.handleInput}
-            />
-            <span className="obligatoire">*</span>
-            <span className="spanInfo">
-              1 Maj, 1 chiffre, 8 - 20 caractères
-            </span>
-          </div>
-          <div>
-            <input
-              id="inputConfirmedPwdInscription"
-              className="inputStyleInscription"
-              type="password"
-              name="confirmedPwd"
-              value={this.state.confirmedPwd}
-              placeholder="Confirme password"
-              required
-              onChange={this.handleInput}
-            />
-            <span className="obligatoire">*</span>
-            <span className="spanInfo">
-              1 Maj, 1min, 1 chiffre, 8 - 20 caractères
-            </span>
-          </div>
-          <button className="btnSub">S'inscrire</button>
-        </form>
+        <MyContext.Consumer>
+          {context => (
+            <form
+              id="formInscription"
+              className="displayFormInscripton"
+              onSubmit={event => context.checkForm(event)}
+            >
+              <h2 className="titleInscription">Inscription</h2>
+              <div>
+                <input
+                  className="inputStyleInscription"
+                  type="text"
+                  name="name"
+                  placeholder="Nom"
+                  value={this.state.name}
+                  required
+                  onChange={this.handleInput}
+                />
+                <span className="obligatoire">*</span>
+                <span className="spanInfo">4 - 12 caractères</span>
+              </div>
+              <div>
+                <input
+                  className="inputStyleInscription"
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  placeholder="E-mail"
+                  required
+                  onChange={this.handleInput}
+                />
+                <span className="obligatoire">*</span>
+                <span className="spanInfo">
+                  De la forme ....@... com | fr | co.uk
+                </span>
+              </div>
+              <div>
+                <input
+                  id="inputPwdInscription"
+                  className="inputStyleInscription"
+                  type="password"
+                  name="pwd"
+                  value={this.state.pwd}
+                  placeholder="Password"
+                  required
+                  onChange={this.handleInput}
+                />
+                <span className="obligatoire">*</span>
+                <span className="spanInfo">
+                  1 Maj, 1 chiffre, 8 - 20 caractères
+                </span>
+              </div>
+              <div>
+                <input
+                  id="inputConfirmedPwdInscription"
+                  className="inputStyleInscription"
+                  type="password"
+                  name="confirmedPwd"
+                  value={this.state.confirmedPwd}
+                  placeholder="Confirme password"
+                  required
+                  onChange={this.handleInput}
+                />
+                <span className="obligatoire">*</span>
+                <span className="spanInfo">
+                  1 Maj, 1min, 1 chiffre, 8 - 20 caractères
+                </span>
+              </div>
+
+              <button className="btnSub">S'inscrire</button>
+            </form>
+          )}
+        </MyContext.Consumer>
       </Fragment>
     );
   }
--- src/contexts/context.js
+++ src/contexts/context.js
@@ -71,6 +71,73 @@ export class MyProvider extends Component {
       .catch(err => console.log('fetch error ' + err.message));
   };
 
+  checkForm = event => {
+    event.preventDefault();
+    let conform = true;
+    let inputs = document
+      .getElementById('formInscription')
+      .getElementsByTagName('input');
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (conform) {
+        conform = inputs[i].className === 'validInput' ? true : false;
+      }
+    }
+
+    if (conform) {
+      if (this.state.pwd === this.state.confirmedPwd) {
+        this.inscription(event);
+      } else {
+        document.getElementById('inputPwdInscription').className =
+          'invalidInput';
+        document.getElementById('inputConfirmedPwdInscription').className =
+          'invalidInput';
+        alert('Mot de passe non confirmé');
+      }
+    } else {
+      alert('Champ(s) mal saisis');
+    }
+  };
+
+  inscription = event => {
+    const FORM_DATA = new FormData(event.target);
+    let jsonObject = {};
+    for (const [key, value] of FORM_DATA.entries()) {
+      jsonObject[key] = value;
+    }
+
+    fetch('http://localhost:4000/mymovies/inscription', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(jsonObject),
+    })
+      .then(() => {
+        this.inscriptionConnexion(jsonObject);
+      })
+      .catch(err => console.log('fetch error ' + err.message));
+  };
+
+  inscriptionConnexion = jsonObject => {
+    fetch('http://localhost:4000/autoconnexion', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(jsonObject),
+    })
+      .then(result => result.json())
+      .then(result => {
+        localStorage.setItem('name', result[0]);
+        localStorage.setItem('idUser', result[1]);
+        window.location.replace('http://localhost:3000/');
+      })
+      .catch(err => console.log('fetch error ' + err.message));
+  };
+
   render() {
     return (
       <MyContext.Provider
@@ -79,6 +146,7 @@ export class MyProvider extends Component {
           logIn: this.logIn,
           logOut: this.logOut,
           connexion: this.connexion,
+          checkForm: this.checkForm,
           deleteUser: this.deleteUser,
         }}
       >
